ca7dab7cbeff9b045ce99507d4a22b8ca83cb83a
JavierGalindoToledo <javier.galindo.toledo13@gmail.com>
2020-10-12 20:08:19 +0300

    first commit

--- test/test_Matrix.cpp
+++ test/test_Matrix.cpp
@@ -1,14 +1,11 @@
 
-
+#include "Matrix.h"
 #include <gtest.h>
-/*
-TEST(TSet, can_get_max_power_set)
+TEST(TMatrix, can_create_matrix)
 {
-  const int size = 5;
-  TSet set(size);
-
-  EXPECT_EQ(size, set.GetMaxPower());
+	ASSERT_NO_THROW(TMatrix<int>(3));
 }
+/*
 
 TEST(TSet, can_insert_non_existing_element)
 {
